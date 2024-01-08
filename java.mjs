2c197edab24275ff2983b7fbfba08e2c71d8b3ec
sheheryarkh <154220737+sheheryarkh@users.noreply.github.com>
2024-01-08 14:39:37 +0500

    Add files via upload

---
+++ java.mjs
@@ -0,0 +1,11 @@
+let welcome ="hello";
+let name ="tayyab";
+let age ="your age is";
+let him  ="4";
+let greeting =" you are cute";
+console.log(welcome);
+console.log(name);
+console.log(age);
+console.log(him);
+console.log(greeting);
+document.querySelector(".sub").innerHTML=("hello tayyab  your age is 4 you are cute");
